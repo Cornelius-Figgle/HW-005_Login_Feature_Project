d3368d6a35c4d5dd30345d5745dcb643f2fb3155
Cornelius-Figgle <max@fullimage.net>
2024-10-09 14:45:26 +0100

    Added validility checks on the password field

--- login.py
+++ login.py
@@ -7,28 +7,9 @@ A test project for my college course.
 
 ## TODO:
 
-1. Ask the user for a username and password. Store these in variables with
-appropriate names. Have clear messages for users and make sure you introduce
-the program to the user so you make sure they know what they are doing.
-2. Allow user to login if the username and password inserted matches a
-previously stored username and password (one you had assigned to appropriately
-named variables). Give the user a message of confirmation or not that username
-and password is correct and login was permitted.
 3. Create a menu of options for your user and display it if the user logins
 successfully. Place this menu in a subroutine/procedure that can be called on
 login happens.
-4. Amend your program to allow user to either register or login at the start
-of your program (after introducing the user to your program). The user should
-input a username, a password and an email address.
-5. Amend your code to allow verification of the password - this means the user
-needs to input the password twice and you need to verify that both are the
-same.
-6. Amend the code to validate the password and make sure it has at least 8
-characters in length and uses capitals, lowercase and numbers.
-7. Amend your code to validate that the user inputted a valid email address and
-ask the user to input again (password or email) if is not valid.
-8. Record username, password and email in a list. Use a 2D list if  you want to
-save more than one set of username/password/email.
 9. Record all the usernames, passwords and emails in a file and amend the code
 to when logging in compare the code to all the entries of the usernames in the
 file. Display messages of incorrect password, users not registered and others,
@@ -239,15 +220,15 @@ class Login:
                 ).encode()
             ).hexdigest()
 
-            # if username is invalid
+            # if username already exists
             if username_hash in userdata:
-                break
-            else:
                 self.InterfaceObj.info(
                     'Account already exists, please use a different name.',
                     error=True
                 )
                 continue
+            else:
+                break
 
         # retrieve a display name
         display_name = self.InterfaceObj.prompt(
@@ -271,7 +252,60 @@ class Login:
                 continue
 
         while True:
-            # retrieve password and hash it
+            # retrieve password
+            password = self.InterfaceObj.prompt(
+                'Password: ',
+                hidden=True
+            )
+
+            # check if password is valid
+            if len(password) < 8:
+                self.InterfaceObj.info(
+                    'Password must be 8 or more characters, please try again.',
+                    error=True
+                )
+                continue
+            elif password.lower() == password:
+                self.InterfaceObj.info(
+                    'Password needs an uppercase, please try again.',
+                    error=True
+                )
+                continue
+            elif password.upper() == password:
+                self.InterfaceObj.info(
+                    'Password needs a lowercase, please try again.',
+                    error=True
+                )
+                continue
+            elif password.isalpha():
+                self.InterfaceObj.info(
+                    'Password needs a number, please try again.',
+                    error=True
+                )
+                continue
+            else:
+                # if all requirements are met, hash the password
+                password_hash = sha256(password.encode()).hexdigest()
+                del password
+                break
+
+
+        # retrieve a confirmation of the password
+        confirm_password_hash = sha256(
+            self.InterfaceObj.prompt(
+                'Confirm Password: ',
+                hidden=True
+            ).encode()
+        ).hexdigest()
+
+        # check if passwords match
+        while password_hash != confirm_password_hash:
+            self.InterfaceObj.info(
+                'Passwords do not match, please try again.',
+                error=True
+            )
+
+            # retrieve password
             password_hash = sha256(
                 self.InterfaceObj.prompt(
                     'Password: ',
@@ -286,16 +320,6 @@ class Login:
                     hidden=True
                 ).encode()
             ).hexdigest()
-
-            # check if password is correct
-            if password_hash != confirm_username_hash:
-                break
-            else:
-                self.InterfaceObj.info(
-                    'Passwords do not match, please try again.',
-                    error=True
-                )
-                continue
 
         # add new user to data
         userdata[username_hash] = dict() 
