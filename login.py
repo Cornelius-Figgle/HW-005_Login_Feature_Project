853780d7a15fbfcd682f70a018ff376543de6bc2
Cornelius-Figgle <max@fullimage.net>
2024-10-10 09:15:59 +0100

    added main menu for after sign in

--- login.py
+++ login.py
@@ -7,14 +7,21 @@ A test project for my college course.
 
 ## TODO:
 
-3. Create a menu of options for your user and display it if the user logins
-successfully. Place this menu in a subroutine/procedure that can be called on
-login happens.
-9. Record all the usernames, passwords and emails in a file and amend the code
-to when logging in compare the code to all the entries of the usernames in the
-file. Display messages of incorrect password, users not registered and others,
-if user not in the file.
+3. Create a menu of options for your user and display it if the user
+logins successfully. Place this menu in a subroutine/procedure that can
+be called on login happens.
 '''
+
+__version__ = '1.0.0'  
+__author__ = 'Cornelius-Figgle'
+__email__ = 'max@fullimage.net'
+__maintainer__ = 'Cornelius-Figgle'
+__copyright__ = 'Copyright (c) 2024 Max Harrison'
+__license__ = 'MIT'
+__status__ = 'Development'
+__credits__ = ['Max Harrison']
+
+# source: https://github.com/Cornelius-Figgle/HW-005_Login_Feature_Project
 
 
 import json
@@ -58,7 +65,7 @@ class Interface:
         if title:
             print(f'# {message_text}\n')
         elif error:
-            print(f'\nERROR: {message_text}\n')
+            print(f'\n\tERROR: {message_text}\n')
         else:
             print(f'{message_text}\n')
 
@@ -81,8 +88,8 @@ class Interface:
 
     def option(self, prompt_text: str, options: list[str]) -> int:
         '''
-        Prompts the user to pick and option from a list, and returns the index
-        of this option.
+        Prompts the user to pick and option from a list, and returns the
+        index of this option.
         '''
 
         # print the question
@@ -99,7 +106,7 @@ class Interface:
             if choice in range(len(options)):
                 break
             else:
-                self.InterfaceObj.info(
+                self.info(
                     'Please choose an option from the list.',
                     error=True
                 )
@@ -111,7 +118,8 @@ class Interface:
 
 class Login:
     '''
-    Collection of methods for generating a login window for an application.
+    Collection of methods for generating a login window for an
+    application.
     '''
 
     def __init__(self) -> None:
@@ -124,10 +132,8 @@ class Login:
 
         # print a title and description
         self.InterfaceObj.info('Login Feature Project', title=True)
-        self.InterfaceObj.info(
-            'Simple login feature that could be implemented into another '
-            +'program.'
-        )
+        self.InterfaceObj.info('Simple login feature that could be implemented'
+            +'into another program.')
 
         # setup data file
         self.userdata_path = os.path.join(
@@ -167,17 +173,24 @@ class Login:
                 ).encode()
             ).hexdigest()
 
-            # if username is invalid
-            if username_hash in userdata:
-                break
-            else:
+            # if username is not valid
+            if username_hash not in userdata:
                 self.InterfaceObj.info(
                     'Account does not exist, please try again.',
                     error=True
                 )
                 continue
+            # if field is blank
+            elif not username_hash:
+                self.InterfaceObj.info(
+                    'Field blank, please enter a username.',
+                    error=True
+                )
+                continue
+            else:
+                break
 
-        while True:
+        for i in range(3):
             # retrieve password and hash it
             password_hash = sha256(
                 self.InterfaceObj.prompt(
@@ -187,15 +200,22 @@ class Login:
             ).hexdigest()
 
             # check if password is correct
-            if password_hash == userdata[username_hash]['password']:
-                break
-            else:
-                self.InterfaceObj.info(
-                    'Password is incorrect, please try again.',
-                    error=True
-                )
+            if password_hash != userdata[username_hash]['password']:
+                if i < 2:
+                    self.InterfaceObj.info(
+                        'Password is incorrect, please try again.',
+                        error=True
+                    )
+                else:
+                    self.InterfaceObj.info(
+                        'Too many incorrect attempts. Exiting progam.',
+                        error=True
+                    )
+                    return 
                 continue
-
+            else:
+                break
+        
         # set current account
         self.current_user = username_hash
         
@@ -224,6 +244,13 @@ class Login:
             if username_hash in userdata:
                 self.InterfaceObj.info(
                     'Account already exists, please use a different name.',
+                    error=True
+                )
+                continue
+            # if field blank
+            elif not username_hash:
+                self.InterfaceObj.info(
+                    'Field blank, please enter a username.',
                     error=True
                 )
                 continue
@@ -338,6 +365,284 @@ class Login:
 
         return
 
+    def main_menu(self) -> None:
+        '''
+        Method for displaying the landing page after a successful login. 
+        '''
+        
+        # get user info from file
+        with open(self.userdata_path, 'r') as userdata_file:
+            userdata = json.load(userdata_file)
+
+        # print welcome message
+        self.InterfaceObj.info(
+            f'Hello {userdata[self.current_user]["display_name"]}!'
+        )
+
+        # loop
+        while True:
+            # print title
+            self.InterfaceObj.info(
+                'Main Menu',
+                title=True
+            )
+
+            # menu
+            choice = self.InterfaceObj.option(
+                'What would you like to do?',
+                ['Account Options', 'Version Info', 'Quit']
+            )
+            match choice:
+                case 0:
+                    self.account_options()
+                case 1:
+                    self.version_info()
+                case 2:
+                    return
+
+    def account_options(self) -> None:
+        '''
+        Method for changing account information.
+        '''
+
+        while True:
+            # get current credentials from file
+            with open(self.userdata_path, 'r') as userdata_file:
+                userdata = json.load(userdata_file)
+
+            # print title
+            self.InterfaceObj.info(
+                'Account Options',
+                title=True
+            )
+
+            # submenu
+            choice = self.InterfaceObj.option(
+                'What would you like to do?',
+                [
+                    'Change Username',
+                    'Change Display Name',
+                    'Change Email Address', 
+                    'Change Password',
+                    'Back to Main Menu'
+                ]
+            )
+            match choice:
+                case 0:
+                    while True:
+                        # retrieve username and hash it
+                        username_hash = sha256(
+                            self.InterfaceObj.prompt(
+                                'Username: '
+                            ).encode()
+                        ).hexdigest()
+
+                        # if username already exists
+                        if username_hash in userdata:
+                            self.InterfaceObj.info(
+                                'Account already exists, please use a'
+                                    +'different name.',
+                                error=True
+                            )
+                            continue
+                        # if field blank
+                        elif not username_hash:
+                            self.InterfaceObj.info(
+                                'Field blank, please enter a username.',
+                                error=True
+                            )
+                            continue
+                        else:
+                            break
+
+                    # move current user details to new username
+                    userdata[username_hash] = userdata[self.current_user].copy()
+                    userdata.pop(self.current_user)
+                    self.current_user = username_hash
+
+                    # write new data to file
+                    with open(self.userdata_path, 'w') as userdata_file:
+                        json.dump(userdata, userdata_file)
+
+                    self.InterfaceObj.info('\nUsername changed successfully.')
+                case 1:
+                    # retrieve a display name
+                    display_name = self.InterfaceObj.prompt(
+                        'Display Name: '
+                    )
+                    
+                    # change stored name
+                    userdata[self.current_user]['display_name'] = display_name
+
+                    # write new data to file
+                    with open(self.userdata_path, 'w') as userdata_file:
+                        json.dump(userdata, userdata_file)
+
+                    self.InterfaceObj.info('\nDisplay Name changed successfully.')
+                case 2:
+                    while True:
+                        # retrieve an email address
+                        email_address = self.InterfaceObj.prompt(
+                            'Email Address: '
+                        )
+
+                        # check is the email is valid
+                        if fullmatch(r'[^@]+@[^@]+\.[^@]+', email_address):
+                            break
+                        else:
+                            self.InterfaceObj.info(
+                                'Email is invalid, please try again.',
+                                error=True
+                            )
+                            continue
+
+                    # change stored email
+                    userdata[self.current_user]['email_address'] = email_address
+
+                    # write new data to file
+                    with open(self.userdata_path, 'w') as userdata_file:
+                        json.dump(userdata, userdata_file)
+
+                    self.InterfaceObj.info('\nEmail Address changed successfully.')
+                case 3:
+                    for i in range(3):
+                        # retrieve password and hash it
+                        password_hash = sha256(
+                            self.InterfaceObj.prompt(
+                                'Current Password: ',
+                                hidden=True
+                            ).encode()
+                        ).hexdigest()
+
+                        # check if password is correct
+                        if password_hash != userdata[self.current_user]['password']:
+                            if i < 2:
+                                self.InterfaceObj.info(
+                                    'Password is incorrect, please try again.',
+                                    error=True
+                                )
+                            else:
+                                self.InterfaceObj.info(
+                                    'Too many incorrect attempts. Exiting progam.',
+                                    error=True
+                                )
+                                return 
+                            continue
+                        else:
+                            break
+
+                    while True:
+                        # retrieve password
+                        password = self.InterfaceObj.prompt(
+                            'New Password: ',
+                            hidden=True
+                        )
+
+                        # check if password is valid
+                        if len(password) < 8:
+                            self.InterfaceObj.info(
+                                'Password must be 8 or more characters, please try again.',
+                                error=True
+                            )
+                            continue
+                        elif password.lower() == password:
+                            self.InterfaceObj.info(
+                                'Password needs an uppercase, please try again.',
+                                error=True
+                            )
+                            continue
+                        elif password.upper() == password:
+                            self.InterfaceObj.info(
+                                'Password needs a lowercase, please try again.',
+                                error=True
+                            )
+                            continue
+                        elif password.isalpha():
+                            self.InterfaceObj.info(
+                                'Password needs a number, please try again.',
+                                error=True
+                            )
+                            continue
+                        else:
+                            # if all requirements are met, hash the password
+                            password_hash = sha256(password.encode()).hexdigest()
+                            del password
+                            break
+
+                    # retrieve a confirmation of the password
+                    confirm_password_hash = sha256(
+                        self.InterfaceObj.prompt(
+                            'Confirm New Password: ',
+                            hidden=True
+                        ).encode()
+                    ).hexdigest()
+
+                    # check if passwords match
+                    while password_hash != confirm_password_hash:
+                        self.InterfaceObj.info(
+                            'Passwords do not match, please try again.',
+                            error=True
+                        )
+
+                        # retrieve password
+                        password_hash = sha256(
+                            self.InterfaceObj.prompt(
+                                'New Password: ',
+                                hidden=True
+                            ).encode()
+                        ).hexdigest()
+
+                        # retrieve a confirmation of the password
+                        confirm_password_hash = sha256(
+                            self.InterfaceObj.prompt(
+                                'Confirm New Password: ',
+                                hidden=True
+                            ).encode()
+                        ).hexdigest()
+                        
+                    # change stored password
+                    userdata[self.current_user]['password'] = password_hash
+
+                    # write new data to file
+                    with open(self.userdata_path, 'w') as userdata_file:
+                        json.dump(userdata, userdata_file)
+
+                    self.InterfaceObj.info('\nPassword changed successfully.')
+                case 4:
+                    return
+
+    def version_info(self) -> None:
+        '''
+        Method for fetching and displaying version info.
+        '''
+
+        # print title
+        self.InterfaceObj.info(
+            'Version Info',
+            title=True
+        )
+
+        # get python version major.minor.micro
+        python_version = '.'.join(
+            str(sys.version_info[i]) for i in range(3)
+        )
+
+        # display
+        self.InterfaceObj.info(
+            f'> {os.path.basename(__file__)} {__version__}' \
+            f'\n> Python {python_version}'
+        )
+
+        while True:
+            # submenu
+            choice = self.InterfaceObj.option(
+                'What would you like to do?',
+                ['Back to Main Menu']
+            )
+            match choice:
+                case 0:
+                    return
+
 
 def main() -> None:
     '''
@@ -352,15 +657,17 @@ def main() -> None:
         'What would you like to do?',
         ['Login', 'Sign Up']
     )
-
     match choice:
         case 0:
             LoginObj.login()
         case 1:
             LoginObj.signup()
-        case _:
-            pass
 
+    # if successful login
+    if LoginObj.current_user:
+        # display program main menu
+        LoginObj.main_menu()
+    
     return
 
 
